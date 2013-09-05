MiCode/Xiaomi_Kernel_OpenSource

ASoC: PCM: Release memory allocated for DAPM list to avoid memory leak
Fix memory leak by releasing the dynamically allocated memory for
DAPM widget list in case of playback.

CRs-fixed: 386917
Change-Id: I6e79da9e5000c861dab6c15832e91f9fe0d5165d
Signed-off-by: Banajit Goswami <bgoswa@codeaurora.org>

sound/soc/soc-pcm.c
@@ -2046,6 +2046,8 @@ int soc_dpcm_runtime_update(struct snd_soc_dapm_widget *widget)
 			be_disconnect(fe, SNDRV_PCM_STREAM_PLAYBACK);
 		}
 
+		fe_path_put(&list);
+
 capture:
 		/* skip if FE doesn't have capture capability */
 		if (!fe->cpu_dai->driver->capture.channels_min)

CAF-staging/msm8916_kernel

sound/soc/soc-pcm.c
@@ -1888,6 +1888,7 @@ int soc_dpcm_runtime_update(struct snd_soc_dapm_widget *widget)
 			dpcm_be_disconnect(fe, SNDRV_PCM_STREAM_PLAYBACK);
 		}
 
+		dpcm_path_put(&list);
 capture:
 		/* skip if FE doesn't have capture capability */
 		if (!fe->cpu_dai->driver->capture.channels_min)