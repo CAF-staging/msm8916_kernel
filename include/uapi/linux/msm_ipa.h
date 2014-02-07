RonGokhale/codeaurora_kernel

msm: ipa: provide ECM connect and disconnect using IPA messaging
This supplements the exiting mechanism based on netlink events.
This is added because in some cases, the netlink events are
not seen at user-space leading to device crash.

Change-Id: I8e34e1ef6556f3d79af84b39174a849c3bd5d29f
Signed-off-by: Ravi Gummadidala <rgummadi@codeaurora.org>

include/linux/msm_ipa.h
@@ -177,6 +177,12 @@ enum ipa_wlan_event {
 	IPA_EVENT_MAX
 };
 
+enum ipa_ecm_event {
+	ECM_CONNECT = IPA_EVENT_MAX,
+	ECM_DISCONNECT,
+	IPA_EVENT_MAX_NUM
+};
+
 /**
  * enum ipa_rm_resource_name - IPA RM clients identification names
  *
@@ -715,6 +721,11 @@ struct ipa_msg_meta {
 struct ipa_wlan_msg {
 	char name[IPA_RESOURCE_NAME_MAX];
 	uint8_t mac_addr[IPA_MAC_ADDR_SIZE];
+};
+
+struct ipa_ecm_msg {
+	char name[IPA_RESOURCE_NAME_MAX];
+	int ifindex;
 };
 
 /**

CAF-staging/msm8916_kernel

Change-Id: I3f13b45a80bb0792b8c6cc36de909f842f4beb83

include/uapi/linux/msm_ipa.h
@@ -216,7 +216,13 @@ enum ipa_wlan_event {
 	IPA_WLAN_EVENT_MAX
 };
 
-#define IPA_EVENT_MAX ((int)IPA_WLAN_EVENT_MAX)
+enum ipa_ecm_event {
+	ECM_CONNECT = IPA_WLAN_EVENT_MAX,
+	ECM_DISCONNECT,
+	IPA_EVENT_MAX_NUM
+};
+
+#define IPA_EVENT_MAX ((int)IPA_EVENT_MAX_NUM)
 
 /**
  * enum ipa_rm_resource_name - IPA RM clients identification names
@@ -972,6 +978,11 @@ struct ipa_wlan_msg_ex {
 	char name[IPA_RESOURCE_NAME_MAX];
 	uint8_t num_of_attribs;
 	struct ipa_wlan_hdr_attrib_val attribs[0];
+};
+
+struct ipa_ecm_msg {
+	char name[IPA_RESOURCE_NAME_MAX];
+	int ifindex;
 };
 
 /**