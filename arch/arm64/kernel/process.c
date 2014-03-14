benji-cannot-code/fast-vc-linux-benchmark-run-3

arm64: use common reboot infrastructure
Commit 7b6d864b48d9 (reboot: arm: change reboot_mode to use enum
reboot_mode) changed the way reboot is handled on arm, which has a
direct impact on arm64 as we share the reset driver on the VE platform.

The obvious fix is to move arm64 to use the same infrastructure.

Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
[catalin.marinas@arm.com: removed reboot_mode = REBOOT_HARD default setting]
Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

arch/arm64/kernel/process.c
@@ -133,7 +133,7 @@ void machine_restart(char *cmd)
 
 	/* Now call the architecture specific reboot code. */
 	if (arm_pm_restart)
-		arm_pm_restart('h', cmd);
+		arm_pm_restart(reboot_mode, cmd);
 
 	/*
 	 * Whoops - the architecture was unable to reboot.

CAF-staging/msm8916_kernel

Change-Id: I94027fe95d060405819dee93c070cd0845c97e18
Git-commit: ff701306cd49aaff80fb852323b387812bc76491
Git-Repo: git://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/
[abhimany@codeaurora.org: minor fix to allow compilation]
Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

arch/arm64/kernel/process.c
@@ -142,7 +142,7 @@ void machine_restart(char *cmd)
 
 	/* Now call the architecture specific reboot code. */
 	if (arm_pm_restart)
-		arm_pm_restart('h', cmd);
+		arm_pm_restart(REBOOT_HARD, cmd);
 
 	/*
 	 * Whoops - the architecture was unable to reboot.